0617aa1c6235b8fb58e1bf2a967800c0b319744b
Paulo Rocha <paulorocha1988@gmail.com>
2020-06-26 14:54:30 +0100

    saxophone getters test passing

--- src/test/java/instrumentsTest/SaxophoneTest.java
+++ src/test/java/instrumentsTest/SaxophoneTest.java
@@ -1,4 +1,29 @@
 package instrumentsTest;
 
+import instruments.Saxophone;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class SaxophoneTest {
+
+    Saxophone saxophone1;
+    Saxophone saxophone2;
+
+    @Before
+    public void before(){
+        saxophone1 = new Saxophone("alto", "black");
+        saxophone2 = new Saxophone("tenor", "pink");
+    }
+
+    @Test
+    public void hasType(){
+        assertEquals("alto", saxophone1.getType());
+    }
+
+    @Test
+    public void hasColour(){
+        assertEquals("black", saxophone1.getColour());
+    }
 }
