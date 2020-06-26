607549516f31de1fcbbea59c6a1b6a815932ea64
Paulo Rocha <paulorocha1988@gmail.com>
2020-06-26 14:35:22 +0100

    guitar getters test passing

--- src/test/java/instrumentsTest/GuitarTest.java
+++ src/test/java/instrumentsTest/GuitarTest.java
@@ -1,4 +1,29 @@
 package instrumentsTest;
 
+import instruments.Guitar;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class GuitarTest {
+
+    Guitar guitar1;
+    Guitar guitar2;
+
+    @Before
+    public void before(){
+        guitar1 = new Guitar("acoustic", "brown");
+        guitar2 = new Guitar("electric", "purple");
+    }
+
+    @Test
+    public void hasType(){
+        assertEquals("electric", guitar2.getType());
+    }
+
+    @Test
+    public void hasColour(){
+        assertEquals("purple", guitar2.getColour());
+    }
 }
