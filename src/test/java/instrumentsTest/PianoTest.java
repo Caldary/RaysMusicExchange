022de329eef4f5733dce48589295d5890c6c75b7
Paulo Rocha <paulorocha1988@gmail.com>
2020-06-26 14:51:00 +0100

    piano getters test passing

--- src/test/java/instrumentsTest/PianoTest.java
+++ src/test/java/instrumentsTest/PianoTest.java
@@ -1,4 +1,29 @@
 package instrumentsTest;
 
+import instruments.Piano;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PianoTest {
+
+    Piano piano1;
+    Piano piano2;
+
+    @Before
+    public void before(){
+        piano1 = new Piano("grand", "black");
+        piano2 = new Piano("electric", "white");
+    }
+
+    @Test
+    public void hasType(){
+        assertEquals("electric", piano2.getType());
+    }
+
+    @Test
+    public void hasColour(){
+        assertEquals("white", piano2.getColour());
+    }
 }
