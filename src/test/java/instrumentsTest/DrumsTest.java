9e3fde33125010974324675b89f074c56fd6ec28
Paulo Rocha <paulorocha1988@gmail.com>
2020-06-26 14:27:52 +0100

    drum getters test passing

--- src/test/java/instrumentsTest/DrumsTest.java
+++ src/test/java/instrumentsTest/DrumsTest.java
@@ -1,4 +1,30 @@
 package instrumentsTest;
 
+
+import instruments.Drums;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class DrumsTest {
+
+    Drums drums1;
+    Drums drums2;
+
+    @Before
+    public void before(){
+        drums1 = new Drums("membranophone","red");
+        drums2 = new Drums("membranophone", "gold");
+    }
+
+    @Test
+    public void hasType(){
+        assertEquals("membranophone", drums1.getType());
+    }
+
+    @Test
+    public void hasColour(){
+        assertEquals("gold", drums2.getColour());
+    }
 }
