2547328789cceefdbc8740c335331c7cbd7217b2
Paulo Rocha <paulorocha1988@gmail.com>
2020-06-26 13:47:39 +0100

    initialized

---
+++ src/main/java/Instruments/Instruments.java
@@ -0,0 +1,13 @@
+package Instruments;
+
+public abstract class Instruments {
+
+    private String type;
+    private String colour;
+
+    public Instruments(String type, String colour){
+        this.type = type;
+        this.colour = colour;
+    }
+
+}
